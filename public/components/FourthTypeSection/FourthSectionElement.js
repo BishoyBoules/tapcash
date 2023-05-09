c4a858cf750deeaada8df8a7c8027382e76c9fb4
Bishoy Boules <79402748+BishoyBoules@users.noreply.github.com>
2023-05-10 00:33:37 +0300

    adding fourth section

--- public/components/FourthTypeSection/FourthSectionElement.js
+++ public/components/FourthTypeSection/FourthSectionElement.js
@@ -1,8 +1,24 @@
 import React from 'react'
 import classes from './FourthSectionElement.module.css'
+import Image from 'next/image'
 
-const FourthSectionElement = () => {
-  return <section className={classes.fourthSectionElement}></section>
+const FourthSectionElement = (props) => {
+  return (
+    <section className={classes.fourthSectionElement}>
+      <section className={classes.firstSection}>
+        <Image src={props.img} alt="img" />
+        <h1>{props.name}</h1>
+      </section>
+      <section className={classes.paragraph}>
+        <p>
+          A distinctive update in the sense of the word. Maybe the best
+          application is a fair application that is presented and a card, but my
+          problem is that it is not satisfied that it enters my account easily
+          and slowly.
+        </p>
+      </section>
+    </section>
+  )
 }
 
 export default FourthSectionElement
--- public/components/FourthTypeSection/FourthTypeSection.js
+++ public/components/FourthTypeSection/FourthTypeSection.js
@@ -1,5 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './FourthTypeSection.module.css'
+import {
+  faChevronLeft,
+  faChevronRight,
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Image from 'next/image'
 import eclipses from '../../assets/Ellipses.png'
 import eclipse1 from '../../assets/Ellipse1.png'
@@ -12,6 +17,58 @@ import eclipse7 from '../../assets/Ellipse7.png'
 import FourthSectionElement from './FourthSectionElement'
 
 const FourthTypeSection = () => {
+  const arr = [
+    {
+      img: eclipse1,
+      name: 'Ahmed Hassan',
+    },
+    {
+      img: eclipse2,
+      name: 'Mahmoud',
+    },
+    {
+      img: eclipse3,
+      name: 'Mostafa',
+    },
+    {
+      img: eclipse4,
+      name: 'Bishoy',
+    },
+    {
+      img: eclipse5,
+      name: 'Marwa',
+    },
+    {
+      img: eclipse6,
+      name: 'Waleed',
+    },
+    {
+      img: eclipse7,
+      name: 'Mayada',
+    },
+  ]
+  const [shownElement, setShownElement] = useState(0)
+  const [lastElement, setLastElement] = useState(false)
+  const [firstElement, setFirstElement] = useState(true)
+  const rightClickHandler = () => {
+    if (shownElement === 5) {
+      setLastElement(true)
+    }
+    if (shownElement != 6) {
+      setFirstElement(false)
+      setShownElement((prevElement) => prevElement + 1)
+      
+    }
+  }
+  const leftClickHandler = () => {
+    if (shownElement === 1) {
+      setFirstElement(true)
+    }
+    if (shownElement != 0) {
+      setLastElement(false)
+      setShownElement((prevElement) => prevElement - 1)
+    }
+  }
   return (
     <section className={classes.fourthTypeSection}>
       <section className={classes.firstSection}>
@@ -20,10 +77,29 @@ const FourthTypeSection = () => {
       </section>
       <section className={classes.secondSection}>
         <section className={classes.sayings}>
-          <FourthSectionElement />
+          <FourthSectionElement
+            img={arr[shownElement].img}
+            name={arr[shownElement].name}
+          />
+        </section>
+        <section className={classes.scroll}>
+          <icon
+            onClick={leftClickHandler}
+            className={classes.icon}
+            style={{ backgroundColor: firstElement ? '#3d3d3f' : '#01a4f1' }}
+          >
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </icon>
+          <icon
+            onClick={rightClickHandler}
+            className={classes.icon}
+            style={{ backgroundColor: lastElement ? '#3d3d3f' : '#01a4f1' }}
+          >
+            <FontAwesomeIcon icon={faChevronRight} />
+          </icon>
         </section>
         <section className={classes.imageSection}>
-          <Image src={eclipses} alt="imageSection" />
+          <Image src={eclipses} alt="imageSection" priority={'true'}/>
         </section>
       </section>
     </section>
